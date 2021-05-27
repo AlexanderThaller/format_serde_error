a4f61771dd349c5dc0413b76e26806b74fa32b7a
Alexander Thaller <alexander.thaller@trivago.com>
2021-05-27 18:31:41 +0200

    Use prettytable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use colored::*;
+use prettytable::{
+    cell,
+    format::FormatBuilder,
+    row,
+    Table,
+};
 
 mod config;
 
@@ -23,62 +29,76 @@ fn print_err(json_str: &str, err: serde_json::Error) -> Result<(), anyhow::Error
         return Err(err.into());
     }
 
-    let lines = json_str.lines().collect::<Vec<_>>();
-    let length = lines.len();
-    let fill = number_length(length);
+    // Amount of lines to show before and after the error line
+    let context = 3;
 
-    let skip = usize::saturating_sub(err.line(), 4);
-    let mut after = err.line() + 3;
-    if after > length {
-        after = length
-    }
+    // Skip until we are amount of context lines before the error line (context)
+    // plus the line with the error ( + 1)
+    // Saturating sub if the error is in the first line we can't take more context
+    let skip = usize::saturating_sub(err.line(), context + 1);
 
-    for (index, line) in lines.into_iter().enumerate().skip(skip) {
-        if index == after {
-            break;
-        }
+    // Take lines before and after (context * 2) plus the line with the error ( + 1)
+    let take = context * 2 + 1;
 
-        if index == err.line() - 1 {
-            println!(
-                " {}{: >fill$} {}{}",
-                err.line().to_string().blue().bold(),
-                "",
-                "|".blue().bold(),
-                line,
-                fill = fill - number_length(index)
-            );
+    // To reduce the amount of space text takes we want to remove unneccessary
+    // whitespace in front of the text.
+    // Find the line with the least amount of whitespace in front and use
+    // that to remove the whitespaces later.
+    // We basically want to find the least indented line.
+    // We cant just use trim as that would remove all whitespace and remove all
+    // indentation.
+    let whitespaces = json_str
+        .lines()
+        .skip(skip)
+        .take(take)
+        .map(|line| line.chars().take_while(|s| s.is_whitespace()).count())
+        .min()
+        .unwrap_or_default();
 
-            println!(
-                " {: >fill$} {}{: >column$}{} {}",
-                "",
-                "|".blue().bold(),
-                "",
-                "^".red().bold(),
-                err.to_string().red().bold(),
-                fill = fill,
-                column = err.column()
-            );
+    let mut table = Table::new();
+    // No padding, or other formatting
+    table.set_format(FormatBuilder::new().build());
+
+    let sepperator = " | ".blue().bold();
+
+    for (line, text) in json_str
+        .lines()
+        .into_iter()
+        .enumerate()
+        .skip(skip)
+        .take(take)
+        // Make the index start at 1 makes it nicer to work with
+        // Also remove whitespaces used for formatting
+        .map(|(index, line)| (index + 1, line.chars().skip(whitespaces).collect::<String>()))
+    {
+        if line != err.line() {
+            // Print context lines
+            table.add_row(row!["", sepperator, text.yellow(),]);
         } else {
-            println!(
-                " {: >fill$} {}{}",
+            // Print error line
+            table.add_row(row![
+                format!(" {}", err.line()).to_string().blue().bold(),
+                sepperator,
+                text,
+            ]);
+
+            // Print error information
+            table.add_row(row![
                 "",
-                "|".blue().bold(),
-                line.yellow(),
-                fill = fill
-            );
+                sepperator,
+                format!(
+                    "{: >column$}^ {}",
+                    "",
+                    err,
+                    column = err.column() - whitespaces
+                )
+                .red()
+                .bold(),
+            ]);
         }
     }
+
+    table.printstd();
 
     Ok(())
-}
-
-fn number_length(mut number: usize) -> usize {
-    let mut len = 0;
-
-    while number > 0 {
-        number /= 10;
-        len += 1;
-    }
-
-    len
 }
